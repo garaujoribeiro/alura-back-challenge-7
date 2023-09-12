afc14736534613f8370f4a38ae5e2ae6770530e5
Gabriel Araujo <gabriel.dev@h4money.com.br>
2023-09-12 18:12:23 -0300

    feat cors enabled

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,6 +15,7 @@ async function bootstrap() {
       },
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
